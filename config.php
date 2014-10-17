5bd3a26c8e9409ca6bebb78569918cd55c5fa8f9
dagnyoskr <dor1@hi.is>
2014-10-17 00:47:59 +0000

    bætt við user php skrám

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-type=application/json; charset=utf-8');
+/*
+ * Upplýsingar um gagnagrunn
+ */
+
+define('DB_USER', "sql554616");
+define('DB_PASSWORD', "tS6%eH6%"); 
+define('DB_DATABASE', "sql554616");
+define('DB_HOST', "sql5.freemysqlhosting.net");
+?>
